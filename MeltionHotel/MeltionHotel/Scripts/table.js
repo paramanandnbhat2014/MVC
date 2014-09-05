4f25e2f2ba625b9a6d1548e9733c565441b1c27f
simon <paramanandbhat14@gmail.com>
2014-09-05 00:54:33 -0700

    Adding Scripts

---
+++ MeltionHotel/MeltionHotel/Scripts/table.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+    $("#table tr:even").addClass("evenRow");
+    $("#table tr:even a").addClass("evenA");
+    $("#table tr:odd a").addClass("oddA");
+
+    $('#table tbody tr a').mouseover(function () {
+        $(this).addClass('evenA');
+    });
+    $('#table tbody tr a').mouseout(function () {
+        $(this).removeClass('evenA');
+    });
+
+    $('#table tbody tr').mouseover(function () {
+        $(this).addClass('dataHover');
+    });
+    $('#table tbody tr').mouseout(function () {
+        $(this).removeClass('dataHover');
+    });
+});
