8d5eaa6f2cb9f36cf8d11e6c2a2466fc59a86e40
simon <paramanandbhat14@gmail.com>
2014-09-05 00:42:32 -0700

    Adding the Models

---
+++ MeltionHotel/MeltionHotel/Models/Customers.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'D:\fit5032\Ass3\MeltionHotel\MeltionHotel\Models\Customers.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
